2c5e634f4064bbc44078096bb6d29fc513e8295f
wohara <wohara@seqsense.com>
2018-07-06 20:13:34 +0900

    Fix but not working

--- src/access_pointcloud.cpp
+++ src/access_pointcloud.cpp
@@ -1,5 +1,5 @@
 #include <ros/ros.h>
-#include <sensor_msgs/point_cloud_iterator.h>
+#include <sensor_msgs/point_cloud2_iterator.h>
 #include <sensor_msgs/PointCloud2.h>
 
 class access_pointcloud
@@ -9,31 +9,28 @@ class access_pointcloud
 
   void Callback(const sensor_msgs::PointCloud2ConstPtr& cloud)
   {
-    sensor_msgs::PointCloud2Modifier modifier(cloud);
-    modifier.setPointCloud2FieldsByString(1, "xyzi");
-    modifier.resize(100);
+    sensor_msgs::PointCloud2Iterator<float> iter_x(*cloud, "x");
+    sensor_msgs::PointCloud2Iterator<float> iter_y(*cloud, "y");
+    sensor_msgs::PointCloud2Iterator<float> iter_z(*cloud, "z");
+    sensor_msgs::PointCloud2Iterator<float> iter_i(*cloud, "intensity");
 
-    sensor_msgs::PointCloud2Iterator<float> iter_x(cloud, "x");
-    sensor_msgs::PointCloud2Iterator<float> iter_y(cloud, "y");
-    sensor_msgs::PointCloud2Iterator<float> iter_z(cloud, "z");
-    sensor_msgs::PointCloud2Iterator<float> iter_z(cloud, "i");
-
-    //n_point 変える
-    for(size_t i=0; i<n_points; ++i, ++iter_x, ++iter_y, ++iter_z, ++iter_i) 
+    for(; iter_x != iter_x.end(); ++iter_x, ++iter_y, ++iter_z, ++iter_i) 
     {
-      *iter_x = point_data[3*i+0];
-      *iter_y = point_data[3*i+1];
-      *iter_z = point_data[3*i+2];
-      *iter_i = color_data[3*i+3];
+      ROS_INFO("x=%f\n",*iter_x);
+      ROS_INFO("y=%f\n",*iter_y);
+      ROS_INFO("z=%f\n",*iter_z);
+      ROS_INFO("intensity=%f\n",*iter_i);
+
+      //今は表示だがmsg型作成してpublish できるようにしたい
     }
   }
 
   public:
-  access_pointcloud
+  access_pointcloud()
   {
     ros::NodeHandle nh("~");
     sub_cloud = nh.subscribe<sensor_msgs::PointCloud2>("cloud",100,
-                                                       &access_pointcloud::Callback)
+                                                       &access_pointcloud::Callback);
   }
 };
 
